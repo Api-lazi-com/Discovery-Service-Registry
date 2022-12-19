8aab53f592f4ed2a52f8d79542502d96a1baf7c4
manh-pham <manhk979@gmail.com>
2022-12-19 18:06:58 +0700

    finish discovery service

--- src/main/java/com/lazi/discoverryserviceregistry/DiscoveryServiceRegistryApplication.java
+++ src/main/java/com/lazi/discoverryserviceregistry/DiscoveryServiceRegistryApplication.java
@@ -2,12 +2,13 @@ package com.lazi.discoverryserviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
-public class DiscoverryServiceRegistryApplication {
+@SpringBootApplication @EnableEurekaServer
+public class DiscoveryServiceRegistryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoverryServiceRegistryApplication.class, args);
+        SpringApplication.run(DiscoveryServiceRegistryApplication.class, args);
     }
 
 }
--- src/test/java/com/lazi/discoverryserviceregistry/DiscoveryServiceRegistryApplicationTests.java
+++ src/test/java/com/lazi/discoverryserviceregistry/DiscoveryServiceRegistryApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoverryServiceRegistryApplicationTests {
+class DiscoveryServiceRegistryApplicationTests {
 
     @Test
     void contextLoads() {
