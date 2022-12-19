d2d5ee6f01dc62d7938b9cfe1af9e5257a5ef446
manh-pham <manhk979@gmail.com>
2022-12-19 17:43:03 +0700

    init

---
+++ src/main/java/com/lazi/discoverryserviceregistry/DiscoveryServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.lazi.discoverryserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverryServiceRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoverryServiceRegistryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lazi/discoverryserviceregistry/DiscoveryServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazi.discoverryserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverryServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
